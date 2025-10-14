43461b81181ca2f707c1bb7202ad0760efa79e7d
czyt1988 <czy.t@163.com>
2025-10-14 23:22:55 +0800

    修复一个异常的初始化

--- src/qwt_plot_parasite_layout.h
+++ src/qwt_plot_parasite_layout.h
@@ -1,4 +1,4 @@
-#ifndef QWTPLOTPARASITELAYOUT_H
+﻿#ifndef QWTPLOTPARASITELAYOUT_H
 #define QWTPLOTPARASITELAYOUT_H
 #include "qwt_global.h"
 #include "qwt_plot_layout.h"
@@ -6,11 +6,11 @@
 class QWT_EXPORT QwtPlotParasiteLayout : public QwtPlotLayout
 {
 public:
-    QwtPlotParasiteLayout();
-    ~QwtPlotParasiteLayout();
-    virtual void activate(const QwtPlot* plot, const QRectF& plotRect, Options options = 0) override;
+	QwtPlotParasiteLayout();
+	~QwtPlotParasiteLayout();
+	virtual void activate(const QwtPlot* plot, const QRectF& plotRect, Options options = Options()) override;
 
-    virtual QSize minimumSizeHint(const QwtPlot* plot) const override;
+	virtual QSize minimumSizeHint(const QwtPlot* plot) const override;
 };
 
 #endif  // QWTPLOTPARASITELAYOUT_H
